94b7e30d83a86d014e3e3dbdb82b33d880f5a70e
sandya <ashanthiya1999@gmail.com>
2026-06-18 22:42:39 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState("");
+
+  const API = "http://localhost:5000";
+
+  const getStudents = async () => {
+    const res = await axios.get(`${API}/students`);
+    setStudents(res.data);
+  };
+
+  const addStudent = async () => {
+    if (!name) return;
+
+    await axios.post(`${API}/students`, {
+      name,
+    });
+
+    setName("");
+    getStudents();
+  };
+
+  const deleteStudent = async (id) => {
+    await axios.delete(`${API}/students/${id}`);
+    getStudents();
+  };
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Student CRUD</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button onClick={addStudent}>
+        Add
+      </button>
+
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name}
+
+            <button
+              onClick={() =>
+                deleteStudent(student.id)
+              }
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
